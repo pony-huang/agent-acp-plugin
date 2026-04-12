efb20b1883802c644d497c98005d6e006f277094
GitHub Action <action@github.com>
2026-04-12 08:14:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/ponyhuang/agentacpplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/ponyhuang/agentacpplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.ponyhuang.agentacpplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
--- src/main/kotlin/com/github/ponyhuang/agentacpplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/ponyhuang/agentacpplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.ponyhuang.agentacpplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.ponyhuang.agentacpplugin.MyBundle
+import com.github.ponyhuang.agentacpplugin.services.MyProjectService
 import javax.swing.JButton
 
 
